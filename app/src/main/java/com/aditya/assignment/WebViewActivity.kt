d2f858f5db58c4dd0566e4721622efbf0a60147c
Aditya <adityasharma131003@gmail.com>
2022-10-02 01:43:23 +0530

    Forward and Backward Browsing

--- app/src/main/java/com/aditya/assignment/WebViewActivity.kt
+++ app/src/main/java/com/aditya/assignment/WebViewActivity.kt
@@ -2,11 +2,15 @@ package com.aditya.assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.aditya.assignment.databinding.ActivityWebViewBinding
 
 class WebViewActivity : AppCompatActivity() {
-    lateinit var binding: ActivityWebViewBinding
+    private lateinit var binding: ActivityWebViewBinding
+    private lateinit var webView : WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,11 +19,36 @@ class WebViewActivity : AppCompatActivity() {
 
         //Taking the Website Link from the user
         val websiteLink : String? = intent.getStringExtra("WebsiteLink")
-
+        webView = binding.webView
         //This prevents WebView to open system default browser
-        binding.webView.webViewClient = WebViewClient()
+        webView.webViewClient = WebViewClient()
+
+        webView.settings.javaScriptEnabled = true
 
         //Loading Website in the webview
-        binding.webView.loadUrl(websiteLink!!)
+        webView.loadUrl(websiteLink!!)
+    }
+
+
+    //Implementing custom menu for the webview activity
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.webview_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //Forward and Backward Browsing Button Handler
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.forward) {
+            if(webView.canGoForward()) {
+                webView.goForward()
+            }
+        } else if(item.itemId == R.id.backward) {
+            if(webView.canGoBack()) {
+                webView.goBack()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
